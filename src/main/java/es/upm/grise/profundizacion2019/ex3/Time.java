44a135d8f15bd54d68a6d87d9ecd3bb13d009cf0
Raul Cruz <raulcrbe@gmail.com>
2020-01-24 16:34:24 +0100

    Fixed Static

--- src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
@@ -3,14 +3,14 @@ package es.upm.grise.profundizacion2019.ex3;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class Time {
-	protected static LocalDateTime time;
+public class Time {	
+	protected  LocalDateTime time;
 	
-	protected Time(LocalDateTime time) {
-		this.time = time;
+	protected Time(LocalDateTime t) {
+		time = t;
 	}
 	
-	public static String getFutureTime(long seconds) {
+	public String getFutureTime(long seconds) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		time.plusSeconds(seconds);
 		System.out.println(formatter.format(time));
