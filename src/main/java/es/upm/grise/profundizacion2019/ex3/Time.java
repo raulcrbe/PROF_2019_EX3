be4a531867cc580018101a39e9ad744ff83b697d
Raul Cruz <raulcrbe@gmail.com>
2020-01-24 16:23:40 +0100

    Extracted Time To A Static Variable

--- src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
@@ -4,11 +4,16 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Time {
+	protected static LocalDateTime time;
+	
+	protected Time(LocalDateTime time) {
+		this.time = time;
+	}
 	
 	public static String getFutureTime(long seconds) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		LocalDateTime time = LocalDateTime.now();
 		time.plusSeconds(seconds);
+		System.out.println(formatter.format(time));
 		return formatter.format(time);	
 	}
 
